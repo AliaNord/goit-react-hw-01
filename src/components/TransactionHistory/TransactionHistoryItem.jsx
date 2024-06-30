1f6352905cf2dac35a983e72b8107ae72695aec4
AliaNord <alia.nord.adventurer@gmail.com>
2024-06-30 20:41:52 +0200

    meow

--- src/components/TransactionHistory/TransactionHistoryItem.jsx
+++ src/components/TransactionHistory/TransactionHistoryItem.jsx
@@ -1,13 +1,13 @@
 import clsx from "clsx";
 import s from "./TransactionHistory.module.css";
 
-const TransactionHistoryItem = ({ type, amount, currency }) => {
+const TransactionHistoryItem = ({ type, amount, currency, id }) => {
   return (
-    <>
+    <tr key={id} className={clsx(s.tr)}>
       <td className={clsx(s.firstCol)}>{type}</td>
       <td>{amount}</td>
       <td>{currency}</td>
-    </>
+    </tr>
   );
 };
 
